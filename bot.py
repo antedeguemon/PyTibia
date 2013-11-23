d528f04198b418175066ba789429cc9941e5f5d8
antedeguemon <scorciapinogeorg@gmail.com>
2013-11-22 23:59:07 -0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,18 +10,7 @@ battle = battle(game)
 #mapa = mapa(game)
 
 location = player.get_location()
-print 'START: '
-print  location
-print
-#y_goto = 32818
-#print float(location[1]-y_goto)
-#mapa.zoom(1)
+print 'walking from: ' + str(location)
+mapa.walk(location, [33201, 32818])
 print battle.monsters()
 
-#mapa.walk(location, [33201, 32818])
-#while True:
-#	location = player.get_location()
-#	if location[0] == 33201:
-#		if location[1] == 32818:
-#			mapa.walk(location, [33200, 32804])
-#	time.sleep(1)
