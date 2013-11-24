9ff8b17ced0ddf80057662dcb1b75156f0b1bddc
root <root@vam-laptop>
2013-11-24 03:07:21 -0200

    penes

--- check.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-im = Image.open('end.png').convert('RGB')
-x, y = im.size
-print 'OFFSET: ' + str(y)
-checksum = ''
-for yy in range(0, y):
-	color = im.getpixel((0, yy))
-	checksum = checksum + str(color[0]) + ',' + str(color[1]) + ',' + str(color[2]) + ';'
-print checksum
