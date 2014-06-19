32755bb79c63d4b7ded64ebc0ac910d34d826ffa
antedeguemon <vicente.merlo@ufrgs.br>
2014-06-19 02:33:54 -0300

    bug fix

--- example_ml.py
+++ example_ml.py
@@ -1,9 +1,11 @@
 from pytibia.tibia import Tibia
 from pytibia.player import player
+from pytibia.battle import battle
 import time
 
 tibia = Tibia()
 player = player(tibia)
+battle = battle(tibia)
 
 while True:
 	if player.mp() > 40:
